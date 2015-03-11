e407abd18c5b9172238458c9e17c1ef37b048af3
DJ Greenfield <mr.kaitryn.dale@gmail.com>
2015-03-11 17:45:08 -0600

    Added Admin feature spec setup.
    Now that the setups for those are finished I have to decide whether to actually write the tests out, help on the database stuff, or do something else. Our foundation is almost complete and once it is, the real work begins.

---
+++ spec/features/admin_dashboard_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+describe 'admin dashboard' do
+  it 'when first logged in' do
+    # 057
+    # As an Admin
+    # When I login
+    # Then I am redirected to '/admin/inventory'
+    # And I see a list of breeds and a list of cats
+  end
+  context 'from the inventory' do
+    it 'links to edit breed' do
+      # 058
+      # As an Admin
+      # When I visit '/admin/inventory'
+      # And I click on an "edit-#{breed.name}" button
+      # Then I am redirected to '/admin/breeds/:id/edit'
+      # And I see information for updating that particular breed
+    end
+
+    it 'links to edit a specific cat' do
+      # 062
+      # As an Admin
+      # When I visit '/admin/inventory'
+      # And I click on an "edit-#{cat.name}" button
+      # Then I am redirected to '/admin/cats/:id/edit'
+      # And I see information for updating that particular cat
+    end
+  end
+end
---
+++ spec/features/admin_order_spec.rb
@@ -0,0 +1,57 @@
+require 'rails_helper'
+describe 'admin orders' do
+  it 'displays a list of orders and filter buttons' do
+    # 066
+    # As an Admin
+    # When I visit '/admin/orders'
+    # Then I see a list of orders with filter buttons to sort them
+  end
+  context 'from "/admin/orders"' do
+    it 'allows admin to change status to "paid"' do
+      # 067
+      # As an Admin
+      # When I visit '/admin/orders'
+      # And I see an order which is ready
+      # And I click on a "paid" button
+      # Then I stay on the same page
+      # And I see the order status change to "paid"
+    end
+
+    it 'allows admin to change status to "completed"' do
+      # 068
+      # As an Admin
+      # When I visit '/admin/orders'
+      # And I see an order which is paid
+      # And I click on a "completed" button
+      # Then I stay on the same page
+      # And I see the order status change to "completed"
+    end
+    it 'allows admin to change status to "cancelled"' do
+      # 069
+      # As an Admin
+      # When I visit '/admin/orders'
+      # And I see an order which is either ready, paid, or completed
+      # And I click on a "cancelled" button
+      # Then I stay on the same page
+      # And I see the order status change to "cancelled"
+    end
+  end
+  context 'when visiting a particular order'
+    it 'it displays a lot of information' do
+      # 070
+      # As an Admin
+      # When I visit '/admin/orders'
+      # And I click on an order's name
+      # Then I am redirected to a '/admin/orders/:id'
+      # And I see a table describing that order
+      # And buttons to edit that order's status (ready, paid, completed, cancelled)
+    end
+    it 'allows admin to change status with filter buttons' do
+      # 071
+      # As an Admin
+      # When I visit '/admin/orders/:id'
+      # The buttons should function as they do in 'admin/orders'
+      # Except they link me to the coordinating '/users/:id/orders'
+    end
+  end
+end
