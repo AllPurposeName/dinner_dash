48bf107e4e91bc69e88ff275ebf1309a586bbb63
DJ Greenfield <mr.kaitryn.dale@gmail.com>
2015-03-11 17:03:38 -0600

    fixed the guest feature setup

--- spec/features/guest_login_spec.rb
+++ spec/features/guest_login_spec.rb
@@ -1,8 +1,15 @@
 require 'rails_helper'
 describe 'Guest on the login page', type: :feature do
 
+
   context 'there is as skip button' do
     it 'login is not necessary' do
+      # 047
+      # As a Guest
+      # When I visit '/login'
+      # And I don't wish to login
+      # And I click the "not now" button
+      # Then I should see the dashboard
     end
 
     it 'logs in with valid data' do
