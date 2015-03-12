fd5569ad11c5654379978079c02d6a56da0a14bc
Valentino Espinoza <xvalentino13@gmail.com>
2015-03-12 17:57:17 -0600

    created Orders so that Users can have an Orders Method
    Orders now have a user and users have many orders. the tests are now passing but not all attributes are being validated.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150312223758) do
+ActiveRecord::Schema.define(version: 20150312235331) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,15 @@ ActiveRecord::Schema.define(version: 20150312223758) do
     t.datetime "updated_at",  null: false
   end
 
+  create_table "orders", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "orders", ["user_id"], name: "index_orders_on_user_id", using: :btree
+
   create_table "users", force: :cascade do |t|
     t.text     "username"
     t.string   "password_digest"
@@ -45,4 +54,5 @@ ActiveRecord::Schema.define(version: 20150312223758) do
     t.datetime "updated_at",      null: false
   end
 
+  add_foreign_key "orders", "users"
 end
---
+++ spec/factories/orders.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :order do
+    user nil
+status 1
+  end
+
+end
