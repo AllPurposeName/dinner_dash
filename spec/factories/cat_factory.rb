0e5999f1b9c926043a96c2c65eba14d12b032668
Valentino Espinoza <xvalentino13@gmail.com>
2015-03-11 18:28:16 -0600

    Wrote all factories except for breeds
    made small changes to test. Corrected cat to imgae ??

---
+++ spec/factories/cat_factory.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :cat do
+    name "cat name"
+    description "example description"
+    price "20.00"
+    status false
+    image_path "IDK"
+    association :breed, factory: :breed, :name "tabby"
+  end
+end
