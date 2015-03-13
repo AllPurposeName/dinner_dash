c1bb18a93f9ffd7d8871a041bf70cf1dae0d0c0d
DJ Greenfield <mr.kaitryn.dale@gmail.com>
2015-03-12 19:49:57 -0600

    Added two user stories to the guest login spec.
    I had forgotten to add a regular login and an invalid login. We will later test with the cart that login from there redirects you, but first we must build out the basic functionality.

--- spec/features/guest_login_spec.rb
+++ spec/features/guest_login_spec.rb
@@ -1,21 +1,31 @@
-# require 'rails_helper'
-# describe 'Guest on the login page', type: :feature do
-#
-#
-#   context 'there is as skip button' do
-#     it 'login is not necessary' do
-#       # 047
-#       # As a Guest
-#       # When I visit '/login'
-#       # And I don't wish to login
-#       # And I click the "not now" button
-#       # Then I should see the dashboard
-#     end
-#
-#     it 'logs in with valid data' do
-#     end
-#
-#     it 'does not log in with invalid data' do
-#     end
-#   end
-# end
+require 'rails_helper'
+describe 'Guest on the login page', type: :feature do
+
+
+  context 'there is as skip button' do
+    it 'login is not necessary' do
+      # 047
+      # As a Guest
+      # When I visit '/login'
+      # And I don't wish to login
+      # And I click the "not now" button
+      # Then I should see the dashboard
+    end
+
+    it 'logs in with valid data' do
+    #   As a Guest
+    #   when I visit '/login'
+    #   And I enter valid information
+    #   And I click submit
+    #   Then I should be redirected to the dashboard as a logged in user
+    end
+
+    it 'does not log in with invalid data' do
+    # As a Guest
+    # When I visit '/login'
+    # And I enter invalid information
+    # And I click Submit
+    # Then I am denied login and prompted to attempt again
+    end
+  end
+end
