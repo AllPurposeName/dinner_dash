a85188be4e2c5bbd8e7a1f345d58c74eaab9772d
Valentino Espinoza <xvalentino13@gmail.com>
2015-03-13 18:42:08 -0600

    Fixed User Spec: Rewrote expectation and fixed rails helper
    Rails Helper didn't need a database cleaner.lint. User spec is currently written to check uniqueness and if it is a plausible email. Uniqueness is strangely not passing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  include ActiveModel::Validations
+
   has_secure_password
   has_many :orders
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}, uniqueness: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,37 +1,42 @@
 require 'rails_helper'
- describe "Database exists and" do
 
-   let(:example_user) { create(:user) }
-   context "has a User that" do
-     it "has a username" do
-       expect(example_user.username).to eq("someusername")
-     end
+describe "Database exists and" do
+  let(:example_user) { create(:user) }
+  context "has a User that" do
+    it "has a username" do
+      expect(example_user.username).to eq("someusername")
+    end
 
-     it "has a password" do
-       expect(example_user.password).to eq("password")
-     end
+    it "has a password" do
+      expect(example_user.password).to eq("password")
+    end
 
-     it "has a full name" do
-       expect(example_user.full_name).to eq("First Last")
-     end
+    it "has a full name" do
+      expect(example_user.full_name).to eq("First Last")
+    end
 
-     it "has a role " do
-       expect(example_user.role).to eq(0)
-     end
+    it "has a role " do
+      expect(example_user.role).to eq(0)
+    end
 
-     it "has an email" do
-       expect(example_user.email).to eq("example@example.com")
-     end
+    describe "has an email" do
+      it "exists" do
+        expect(example_user.email).to eq("example@example.com")
+      end
 
-     it "has an email that is unique and plausible" do
-       user_with_invalid_email = create(:user, email: "poop")
-       expect(User.find(example_user.id)).to eq(example_user)
-       expect(User.find(user_with_invalid_email.id)).to eq(false)
-     end
+      xit "is unique" do
+        duplicate_email_user = build(:user)
+        expect(duplicate_email_user).to_not be_valid
+      end
 
-     it "has orders" do
-       expect(example_user.orders).to eq([])
-     end
-   end
- end
+      it "is plausible" do
+        expect(build(:user, email: "Poop")).to_not be_valid
+      end
+    end
+
+    it "has orders" do
+      expect(example_user.orders).to eq([])
+    end
+  end
+end
 
