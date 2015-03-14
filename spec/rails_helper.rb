aa4cfa96dce2e6a40f7c29323f53006c2a094b7c
Valentino Espinoza <xvalentino13@gmail.com>
2015-03-14 11:45:05 -0600

    Fixed line indentation in rails helper
    there were two lines that were indented too far

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -43,11 +43,11 @@ RSpec.configure do |config|
     DatabaseCleaner.clean_with(:truncation)
   end
 
-#  config.around(:each) do |example|
-#    DatabaseCleaner.cleaning do
-#      example.run
-#    end
-#  end
+  #  config.around(:each) do |example|
+  #    DatabaseCleaner.cleaning do
+  #      example.run
+  #    end
+  #  end
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
