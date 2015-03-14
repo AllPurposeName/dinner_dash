33ebf641dc896f14ba014b469e74009cc498d5f9
Valentino Espinoza <xvalentino13@gmail.com>
2015-03-14 11:56:23 -0600

    Broke apart validate line in user model
    broke uniqueness too its own line since Hound was trippin'

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ActiveRecord::Base
 
   has_secure_password
   has_many :orders
-  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}, uniqueness: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i},
+    uniqueness: true
 end
