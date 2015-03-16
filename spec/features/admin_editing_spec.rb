1d8d348a3a68da9a0d9584c0afe9452d6a51c96d
DJ Greenfield <mr.kaitryn.dale@gmail.com>
2015-03-16 17:11:42 -0600

    Wrote first edit test for breed.
    An unrelated test is failing, I'm goign to fix it before continuing.

--- spec/features/admin_editing_spec.rb
+++ spec/features/admin_editing_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 describe "admin editing mode" do
   context "for breeds" do
-    it "allows admin to change a name and description" do
+    it "allows admin to change all fields" do
       # 059
       # As an Admin
       # When I visit "/admin/breeds/:id/edit"
@@ -10,6 +10,21 @@ describe "admin editing mode" do
       # And I click the "save" button
       # Then I am redirected to that breed"s page
       # And I see the edited breed with it"s new name and description
+      create(:user, role: 1)
+      breed = create(:breed)
+      visit edit_admin_breed(breed)
+
+      fill_in "edit-name", with: "test-name"
+      fill_in "edit-description", with: "test-description"
+      fill_in "edit-image_path", with: "test-image_path"
+      fill_in "edit-price", with: "test-description"
+      fill_in "edit-retired", with: "test-retired"
+      click_link_or_button("Submit")
+
+      within("#flash-editcomplete") do
+        expect(page).to have_content("#{breed.name.capitalize} edited!")
+      end
+      expect(current_path).to eq("/breeds/#{breed.to_param.name}")
     end
     it "allows admin to change image" do
       # 060
