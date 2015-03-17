b94e385a8b34a5a725d26b1b463314964cab411f
DJ Greenfield <mr.kaitryn.dale@gmail.com>
2015-03-17 00:22:43 -0600

    hound stuff

--- spec/features/admin_editing_spec.rb
+++ spec/features/admin_editing_spec.rb
@@ -77,22 +77,5 @@ describe "admin editing mode" do
         expect(page).to have_content("Perci the cat edited!")
       end
     end
-    it "allows admin to change image" do
-      # 064
-      # As an Admin
-      # When I visit "/admin/cats/:id/edit"
-      # And I fill in the image field
-      # And I click the "save" button
-      # Then I am redirected to that cat"s page
-      # And I see the edited cat with it"s new image
-    end
-    it "allows admin to change sold out status" do
-      # 065
-      # As an Admin
-      # When I visit "/admin/cats/:id/edit"
-      # And I click the retire button
-      # Then I am redirected to the cat"s page
-      # And I see the retried cat with it"s status set to "sold out"
-    end
   end
 end
