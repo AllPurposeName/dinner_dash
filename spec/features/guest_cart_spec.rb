9edb8261f8a128d56137446ecd273642d574ad55
Valentino Espinoza <xvalentino13@gmail.com>
2015-03-17 16:14:02 -0600

    continue needed to be a button with a get request instead of post
    had to tweak test so sad paths can work

--- spec/features/guest_cart_spec.rb
+++ spec/features/guest_cart_spec.rb
@@ -14,7 +14,7 @@ describe "guest can use a cart" do
     click_link_or_button("add to cart")
     expect(current_path).to eq(cart_path)
     click_link_or_button("continue")
-    expect(current_path).to eq("/cats/#{kitty.id}")
+    expect(current_path).to eq("/breeds")
   end
 
   it "displays multiple cats" do
