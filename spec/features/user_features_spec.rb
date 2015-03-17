827537f1274f2ec948344b9060b7ab207537c3c1
Valentino Espinoza <xvalentino13@gmail.com>
2015-03-17 12:57:55 -0600

    I wrote the test for add to cart for some reason

--- spec/features/user_features_spec.rb
+++ spec/features/user_features_spec.rb
@@ -1,24 +1,22 @@
 require "rails_helper"
 describe "authenticated user" do
   xit "is not prompted to login when visiting '/user/id/orders'" do
-    # 054
-    # As an authenticated User
-    # When I visit "/user/myid/orders"
-    # Then I am not prompted to login and I see my order list.
   end
 
-  xit "gains items in cart when clicking the 'add to cart' button" do
-    # 055
-    # As an authenticated User
-    # When I click any "add to cart" button
-    # Then I am redirected to "/cart" with my newest order displayed
+  it "gains items in cart when clicking the 'add to cart' button" do
+    cat = create(:cat)
+    user = User.create(role: 0, username: "guy", password: "pass", full_name: "DJ G", email: "example@example.com",)
+    visit login_path
+    fill_in "session[username]", with: "guy"
+    fill_in "session[password]", with: "pass"
+    click_link_or_button("log in")
+    visit cat_path(cat.id)
+    click_link_or_button("add to cart")
+    expect(page).to have_content(cat.price)
+    expect(page).to have_content("cart")
   end
 
   xit "still restricts user from viewing admin or missing templates" do
-    # 056
-    # As an authenticated User
-    # When I visit any location not listed previously
-    # Then I am redirected to dashboard with no flash message
   end
 
   it "is able to checkout their cart" do
@@ -51,7 +49,6 @@ describe "authenticated user" do
     order = user.orders.create(status: 0)
     order.order_cats.create(cat_id: cat.id, quantity: 2)
     visit orders_path
-    save_and_open_page
     expect(page).to have_content(cat.name)
   end
 end
