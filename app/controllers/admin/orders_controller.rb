9ea939e0af77a96a664e98949c6f5b688ff5df02
Valentino Espinoza <xvalentino13@gmail.com>
2015-03-18 17:27:17 -0600

    Admin Index Page Has jQuery Now
    WHOOOOO

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,5 +1,8 @@
 class Admin::OrdersController < ApplicationController
+  respond_to :html, :json
   def index
-
+    @orders = Order.all
+    @statuses = @orders.map {|order| order.status}.uniq
+    respond_with @orders
   end
 end
