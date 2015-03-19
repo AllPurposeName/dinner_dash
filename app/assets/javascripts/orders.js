b174f26c8c91624563de2215316631e3c06bbfdf
Valentino Espinoza <xvalentino13@gmail.com>
2015-03-18 19:01:36 -0600

    Single quotes to double quotes

--- app/assets/javascripts/orders.js
+++ app/assets/javascripts/orders.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
     var currentStatus = this.value;
     $orders.each(function (index, order) {
       $order = $(order);
-      if ($order.data('status') === currentStatus || currentStatus === "all") {
+      if ($order.data("status") === currentStatus || currentStatus === "all") {
         $order.show();
       } else {
         $order.hide();
