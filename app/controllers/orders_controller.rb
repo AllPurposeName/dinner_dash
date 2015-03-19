99b814a0c02ba951cec808c0ffe35d12722bbcb1
Fred Block <info@fredblock.com>
2015-03-18 21:22:35 -0600

    Add monifier to views and updated tie zone.
    Add monifier to 'app/controllers/orders_controller.rb', 'app/views/admin/orders/index.html.erb', 'app/views/orders/index.html.erb', and 'app/views/orders/show.html.erb'. Updated time zone in 'config/application.rb'.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,10 +3,12 @@ class OrdersController < ApplicationController
   def index
     authorize! :read, Order
     @orders = current_user.orders
+    @monifier = Monifier.new
   end
 
   def show
     @order = Order.find(params[:id])
+    @monifier = Monifier.new
   end
 
 end
