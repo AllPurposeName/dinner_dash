b21286f14c4f70868f95b5338168c5f81310136a
Valentino Espinoza <xvalentino13@gmail.com>
2015-03-18 23:32:00 -0600

    admin can now update status off of the order index page
    WITH JQUERY

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,9 +1,15 @@
 class Admin::OrdersController < ApplicationController
   respond_to :html, :json
   def index
-    @orders = Order.all
+    @orders = Order.all.sort
     @statuses = @orders.map(&:status).uniq
     @statuses.unshift("all")
     respond_with @orders
+  end
+
+  def update
+    @order = Order.find(params[:id])
+    @order.update(status: params[:status])
+    render :json => "we good!", :status => :ok
   end
 end
