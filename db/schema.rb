ae85c6e32a8ca2033fb19815ece1cfa1c05c02d6
DJ Greenfield <mr.kaitryn.dale@gmail.com>
2015-03-19 00:43:45 -0600

    Seed data updated.
    In this commit we created a migration to change the default of cat.image_path. We then added logic to the cat breed to set the default cat image to the breed image if none is initialized. We also added validations to the relationships of breed and cats. The main goal and effect of this commit is to create the seed data with random cool stuff. We implemented it mostly, but still have to tweak the names and un-lorem ipsum the descriptions.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150316232131) do
+ActiveRecord::Schema.define(version: 20150319030809) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,10 +36,10 @@ ActiveRecord::Schema.define(version: 20150316232131) do
     t.text     "name"
     t.integer  "price"
     t.boolean  "retired"
-    t.string   "image_path",  default: "cat.jpg"
+    t.string   "image_path"
     t.text     "description"
-    t.datetime "created_at",                      null: false
-    t.datetime "updated_at",                      null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "order_cats", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,65 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-  def random_first_name
-    [:first_name, :prefix].sample
-  end
-
-  def random_last_name
-    [:last_name, :first_name, :suffix].sample
-  end
-
-  def cat_first_name
-    (Faker::Name.send(random_first_name)
-  end
-
-  def cat_last_name
-    Faker::Name.send(random_last_name
-  end
-
-  def cat_full_name
-    "#{cat_first_name} #{cat_last_name}"))
-  end
-
-  15.times do |iteration|
-    User.all.random.order.create(
-      { status: [(1..3)].sample })
-
-  def breed_name
-    if @breed_list
-      breed_list
-    else
-      breed_list_populate
-    end
-  end
-
-  def breed_name
-    @breed_list.shuffle.pop
-  end
-
-  def breed_listing
-    @breed_list = ["Donskoy", "American Bobtail",
-   "Havanna Brown", "Ocicat",
-   "Oriental Shorthair", "Savannah",
-   "Australian Mist", "British Shorthair",
-   "Cyprus", "Russian Black"]
- end
-  breeds = 10.times do |iteration|
-    Breed.create({name: breed_name,
-                  retired: false
-                  description: Faker::Lorem.sentences(7))}
-
-  40.times do |iteration|
-    breeds.sample.cats.create({
-              name: cat_name,
-              price: ((1..50).to_a.sample.to_s + "00").to_i,
-              retired: false,
-              description: Faker::Loren.sentences(10),
-              image_path: "place_holder"
-              })
-  exit
-  
-
 User.create([
   { username:"dj",
     password: "dj",
@@ -108,76 +49,104 @@ User.create([
     full_name: "Josh Cheek",
     email: "demo+josh@jumpstartlab.com" }])
 
-Breed.create([
-  { name: "sabertooth",
-    retired: false,
-    description: "A tasty large fanged feline once thought to be extinct. Don't worry, we'll get the job done." },
 
-  { name: "prionailurus viverrinus",
-    retired: false,
-      description: "This cat lives near marshes, mangroves, rivers and streams in India and Southeast Asia. Our finest import."},
+  def users_minus_dj
+    users_minus_dj = User.all - [User.first]
+  end
 
-  { name: "oncilla",
-    retired: false,
-    description: "This small spotted cat's meat is lean and tender. Goes great between two slices of our signature Rye bread." },
+  users_minus_dj.each do |user|
+    5.times do |iteration|
+      user.orders.create({ status: [(0..3)].sample })
+    end
+  end
 
-  { name: "oriental shorthair",
-    retired: false,
-    description: "Like the Siamese, Oriental Shorthairs have almond-shaped eyes, a triangular head shape, large ears, and an elongated, slender, and muscular body. Excellent for an appetizer." },
+  class BreedMaker
+    attr_accessor :breed_list
 
-  { name: "black cat",
-    retired: false,
-    description: "Halloween is always just around the corner. Fill up on one of these cheap tickets and we'll let you keep the... remains." }])
+    def initialize
+      @breed_list = ["Donskoy", "American Bobtail",
+     "Havanna Brown", "Ocicat",
+     "Oriental Shorthair", "Savannah",
+     "Australian Mist", "British Shorthair",
+     "Cyprus", "Russian Black"]
+    end
 
-Breed.find_by(name: "sabertooth").cats.create([
-  { name:"sabertooth 1",
-    price: 1000,
-    retired: false,
-    description: "cool cat 1" },
+    def breed_name
+      @breed_list.delete(@breed_list.sample)
+    end
 
-    { name:"sabertooth 2",
-      price: 2000,
-      retired: false,
-      description: "cool cat 2" }])
+    def run
+      10.times do |iteration|
+      Breed.create({name: breed_name,
+                    retired: false,
+                    description: Faker::Lorem.sentences(2)})
+      end
+    end
+  end
 
-Breed.find_by(name: "oncilla").cats.create([
-  { name:"oncilla 1",
-    price: 2000,
-    retired: false,
-    description: "cool cat 3" },
+  BreedMaker.new.run
 
-    { name:"oncilla 2",
-      price: 3000,
-      retired: false,
-      description: "cool cat 4" }
-])
 
-Breed.find_by(name: "oriental shorthair").cats.create([
-  { name:"oriental shorthair 1",
-    price: 4000,
-    retired: false,
-    description: "cool cat 5" },
+class CatMaker
+  def random_first_name
+    [:first_name, :prefix].sample
+  end
 
-    { name:"oriental shorthair 2",
-      price: 5000,
-      retired: false,
-      description: "cool cat 6" }
-])
+  def random_last_name
+    [:last_name, :first_name, :suffix].sample
+  end
 
-Breed.find_by(name: "black cat").cats.create([
-  { name:"black cat 1",
-    price: 3500,
-    retired: false,
-    description: "cool cat 7" },
+  def cat_first_name
+    Faker::Name.send(random_first_name)
+  end
 
-    { name:"black cat 2",
-      price: 4000,
-      retired: false,
-      description: "cool cat 8" }
-])
+  def cat_last_name
+    Faker::Name.send(random_last_name)
+  end
 
-Breed.find_by(name: "prionailurus viverrinus").cats.create(
-  { name:"fishing cat 1",
-    price: 1000,
-    retired: false,
-    description: "cool cat 9" })
+  def cat_full_name
+    "#{cat_first_name} #{cat_last_name}"
+  end
+
+  def cents
+    ["00", "50"].sample
+  end
+
+  def dollars
+    common = [10, 12, 15,].sample
+    uncommon = [20, 25, 30, 42].sample
+    rare = [100, 150, 125].sample
+    
+  end
+
+  def run
+    40.times do |iteration|
+      Breed.all.sample.cats.create({
+                name: cat_full_name,
+                price: ((1..50).to_a.sample.to_s + cents).to_i,
+                retired: false,
+                description: Faker::Lorem.sentences(2),
+                image_path: nil
+                })
+    end
+  end
+end
+
+CatMaker.new.run
+
+  def random_cat
+    (1..Cat.count).to_a.sample
+  end
+
+  def random_quantity
+    (1..4).to_a.sample
+  end
+
+  def random_order_cat
+    users_minus_dj.sample.orders.all.sample.order_cats
+  end
+
+  35.times do |iteration|
+    random_order_cat.create({ cat_id: random_cat,
+                              quantity: random_quantity})
+  end
